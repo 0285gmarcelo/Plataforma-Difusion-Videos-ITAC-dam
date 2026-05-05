99c722c1e871e29472d17ade1f81a8548cae821b
0285gmarcelo <0285gmarcelo@e-itaca.es>
2026-05-05 18:56:05 +0200

    Add files via upload

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,20 @@
+package Main;
+import Pantallas.*;
+/**
+ *
+ * @author isard
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MenúInicio prueba = new MenúInicio();
+        prueba.setVisible(true);
+        // hola
+        //prueba
+    }
+    
+}
